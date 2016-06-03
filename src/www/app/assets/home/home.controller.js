42e0fef74c427737edf07d2c1a8432593913d2a2
David Cinadr <dcinadr@gmail.com>
2016-06-02 22:01:18 -0700

    Fixed spacing of question results

--- src/www/app/assets/home/home.controller.js
+++ src/www/app/assets/home/home.controller.js
@@ -1,50 +1,45 @@
-﻿(function () {
-    'use strict';
+﻿
+(function() {
+  'use strict';
 
-    angular
-        .module('youdle' )
-        .controller('homeController', homeController);
+  angular
+    .module('youdle')
+    .controller('homeController', homeController);
 
-    homeController.$inject = ['homeFactory', 'localStorageFactory', 'loggerFactory'];
+  homeController.$inject = ['homeFactory', 'localStorageFactory', 'loggerFactory'];
 
-    function homeController(homeFactory, localStorageFactory, loggerFactory) {
+  function homeController(homeFactory, localStorageFactory, loggerFactory) {
 
-        var vm = this;
-        vm.title = 'Home';
+    var vm = this;
+    vm.title = 'Home';
 
-        activate();
+    activate();
 
-        function activate()
-        {
-          homeFactory.getCards()
-            .then(function(response)
-            {
-              // TODO - not sure that using the homeModel is really a good idea
-              vm.cards = homeFactory.homeModel.cards;
-            },
-            function(errors)
-            {
-              // TODO - display something on page that something went wrong
-              console.error('problem loading cards', errors);
-            }
-          );
-        }
-
-        vm.selectOption = function(card, option)
-        {
-          card.showResults = true;
-          var userObjectId = localStorageFactory.get('userObjectId');
-          var userToken = localStorageFactory.get('userToken');
-          homeFactory.selectOption(option.objectId, card.objectId, userObjectId, userToken)
-            .then(function(response)
-            {
-              response.showResults = true;
-              angular.copy(response, card);
-            },
-            function(errors)
-            {
-              console.error('problem selecting option', errors);
-            });
-        }
+    function activate() {
+      homeFactory.getCards()
+        .then(function(response) {
+            // TODO - not sure that using the homeModel is really a good idea
+            vm.cards = homeFactory.homeModel.cards;
+          },
+          function(errors) {
+            // TODO - display something on page that something went wrong
+            console.error('problem loading cards', errors);
+          }
+        );
     }
+
+    vm.selectOption = function(card, option) {
+      card.showResults = true;
+      var userObjectId = localStorageFactory.get('userObjectId');
+      var userToken = localStorageFactory.get('userToken');
+      homeFactory.selectOption(option.objectId, card.objectId, userObjectId, userToken)
+        .then(function(response) {
+            response.showResults = true;
+            angular.copy(response, card);
+          },
+          function(errors) {
+            console.error('problem selecting option', errors);
+          });
+    }
+  }
 })();
