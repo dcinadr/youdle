3371973d71b6d7c2e6cfa6f24787b1fd1ec80ac1
David Cinadr <dcinadr@gmail.com>
2016-06-09 22:50:17 -0700

    added facebook api; cleaned up some sass css

---
+++ src/www/app/shared/api/api.facebook.login.js
@@ -0,0 +1,35 @@
+﻿
+(function ()
+{
+    'use strict';
+
+    angular
+      .module('youdle.api')
+      .factory('facebookLoginApiFactory', facebookLoginApiFactory);
+
+    facebookLoginApiFactory.$inject = ['$http', '$q'];
+
+    function facebookLoginApiFactory(config)
+    {
+        var service = {
+            post: post
+        };
+
+        function post(data)
+        {
+            return $http({
+                method: 'POST',
+                url: 'https://api.backendless.com/v1/users/social/oauth/facebook/request_url',
+                headers: {
+                    'application-id': 'E11DA057-CE8C-0C31-FF22-59965520EB00',
+                    'secret-key': '6E12B29C-A78E-4C99-FFFA-698C1EE7D200',
+                    'application-type': 'REST',
+                    'content-type': 'application/json'
+                },
+                data: data
+            });
+        }
+
+        return service;
+    }
+})();
